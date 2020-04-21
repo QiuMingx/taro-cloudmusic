17814dbf3f02010b83b1c9878dbe1bc957ed3808
UIBE <285486683@qq.com>
2020-04-21 22:41:42 +0800

    update list

--- src/components/PlayList/index.tsx
+++ src/components/PlayList/index.tsx
@@ -1,7 +1,8 @@
 import Taro, { FC } from '@tarojs/taro'
 import { View, Slider } from '@tarojs/components'
-
+import { AtTabBar, AtSearchBar, AtIcon } from 'taro-ui'
 import './index.less'
+import 'taro-ui/dist/style/index.scss'
 
 type Props = {
   source: Array<{
@@ -21,6 +22,7 @@ type Props = {
    }
   return (
     <View className='recommend_playlist__content'>
+      <Text className='at-icon at-icon-sound'></Text>
       {
         source&&source.map(item =>
           <View className='recommend_playlist__item' key={item.id} onClick={()=>this.props.goDetail(item)}>
@@ -28,11 +30,13 @@ type Props = {
                 src={item.picUrl+'?imageView&thumbnail=0x200'}
                 className='recommend_playlist__item__img'
               />
-              <View className='recommend_playlist__item__cover__num'>
-                 <Text className='at-icon at-icon-sound'></Text>
-                {formatPlayCount(item.playCount)}
-              </View>
-              <View className='recommend_playlist__item__title'>{item.name}</View>
+            <View className='recommend_playlist__item__cover__num'>
+              <Text className='at-icon at-icon-play'></Text>
+                          <Text className='at-icon at-icon-sound'></Text>
+              <AtIcon value='play' size='14' color='#fff'></AtIcon>
+              {formatPlayCount(item.playCount)}
+            </View>
+            <View className='recommend_playlist__item__title'>{item.name}</View>
           </View>)
       }
     </View>
